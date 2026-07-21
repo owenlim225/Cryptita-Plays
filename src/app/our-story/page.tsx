cf5736cd0be23e376b54dabf4412a9aacd2535f6
Owen <87555304+owenlim225@users.noreply.github.com>
2026-07-21 11:23:57 +0800

    restart

--- src/app/our-story/page.tsx
+++
@@ -1,5 +0,0 @@
-import { OurStoryPage } from "@/features/our-story/our-story-page";
-
-export default function Page() {
-  return <OurStoryPage />;
-}
--- src/components/sections/events-section.tsx
+++
@@ -1,8 +0,0 @@
-export function EventsSection() {
-  return (
-    <section className="hidden" aria-hidden>
-      <div className="mx-auto w-full max-w-6xl">
-      </div>
-    </section>
-  );
-}
